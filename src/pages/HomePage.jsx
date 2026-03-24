a9ec2787f6a8e5cdf4f93eeb7689b19d582e152a
8183explorer <8183explorer@users.noreply.github.com>
2026-03-24 20:21:44 +0700

    feat: add pages layer (HomePage)

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import ProblemSection from '../components/ProblemSection';
+import VerificationSection from '../components/VerificationSection';
+import TrustScoreSection from '../components/TrustScoreSection';
+import MonitoredEntities from '../components/MonitoredEntities';
+import TokenTiers from '../components/TokenTiers';
+import Footer from '../components/Footer';
+
+export default function HomePage() {
+  return (
+    <div className="font-sans antialiased selection:bg-yellow selection:text-black">
+      <Navbar />
+      <HeroSection />
+      <ProblemSection />
+      <VerificationSection />
+      <TrustScoreSection />
+      <MonitoredEntities />
+      <TokenTiers />
+      <Footer />
+    </div>
+  );
+}
